089f96a71145438f44f5cdc5bd076bea3117ed26
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 19:56:01 +0530

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,33 @@
 
 import React, { useContext } from 'react';
 import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import { AuthContext } from './contexts/AuthContext';
 import LoginScreen from './screens/LoginScreen';
 import RegistrationScreen from './screens/RegistrationScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import AdminUserDetailsScreen from './screens/AdminUserDetailsScreen';
 
-const Stack = createStackNavigator();
-
 const App: React.FC = () => {
   const { user } = useContext(AuthContext);
 
-  console.log('Rendering App component...');
+  console.log('User:', user);
 
   return (
-    <NavigationContainer>
+    <View>
       {user ? (
-        <Stack.Navigator>
-          <Stack.Screen name="Profile" component={ProfileScreen} />
-          <Stack.Screen name="AdminUserDetails" component={AdminUserDetailsScreen} />
-        </Stack.Navigator>
+        <>
+          <Text>Welcome, {user.name}!</Text>
+          <ProfileScreen />
+          <AdminUserDetailsScreen />
+        </>
       ) : (
-        <Stack.Navigator>
-          <Stack.Screen name="Login" component={LoginScreen} />
-          <Stack.Screen name="Registration" component={RegistrationScreen} />
-        </Stack.Navigator>
+        <>
+          <Text>Please log in or register.</Text>
+          <LoginScreen />
+          <RegistrationScreen />
+        </>
       )}
-    </NavigationContainer>
+    </View>
   );
 };
 
