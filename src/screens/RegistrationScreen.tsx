bfad71f277eca7349c3cfaab5b0353ce628183e5
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 19:49:06 +0530

    First Commit

--- src/screens/RegistrationScreen.tsx
+++ src/screens/RegistrationScreen.tsx
@@ -1,45 +1,73 @@
 
 import React, { useState } from 'react';
-import { View, TextInput, Button } from 'react-native';
+import { View, TextInput, Button, Alert } from 'react-native';
+import { RegistrationFormData, registerUser, validateEmail } from '../apis/AuthApi';
 
-const RegistrationScreen = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+const RegistrationScreen: React.FC = () => {
+  const [formData, setFormData] = useState<RegistrationFormData>({
+    name: '',
+    email: '',
+    password: '',
+  });
 
-  const handleRegistration = () => {
-    console.log('Step 2: User submits the registration form');
-    console.log('Name:', name);
-    console.log('Email:', email);
-    console.log('Password:', password);
-
-    // Perform API call for user registration here
-
-    // Assuming the API call is successful
-    console.log('Registration successful');
+  const handleInputChange = (key: keyof RegistrationFormData, value: string) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [key]: value,
+    }));
   };
 
-  console.log('Step 1: User visits the registration page');
+  const handleRegister = async () => {
+    console.log('Registering user...');
+
+    // Validate email
+    if (!validateEmail(formData.email)) {
+      console.log('Invalid email');
+      Alert.alert('Invalid email', 'Please enter a valid email address');
+      return;
+    }
+
+    try {
+      // Call the registerUser API to register the user
+      const newUser = await registerUser(formData);
+      console.log('User registered successfully:', newUser);
+
+      // Show success message to the user
+      Alert.alert('Registration successful', 'You have successfully registered!');
+
+      // Reset the form data
+      setFormData({
+        name: '',
+        email: '',
+        password: '',
+      });
+    } catch (error) {
+      console.error('Error registering user:', error);
+
+      // Show error message to the user
+      Alert.alert('Registration failed', 'An error occurred while registering. Please try again later.');
+    }
+  };
 
   return (
     <View>
       <TextInput
         placeholder="Name"
-        value={name}
-        onChangeText={setName}
+        value={formData.name}
+        onChangeText={(value) => handleInputChange('name', value)}
       />
       <TextInput
         placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
+        value={formData.email}
+        onChangeText={(value) => handleInputChange('email', value)}
       />
       <TextInput
         placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
         secureTextEntry
+        value={formData.password}
+        onChangeText={(value) => handleInputChange('password', value)}
       />
-      <Button title="Register" onPress={handleRegistration} />
+      <Button title="Register" onPress={handleRegister} />
     </View>
   );
 };
