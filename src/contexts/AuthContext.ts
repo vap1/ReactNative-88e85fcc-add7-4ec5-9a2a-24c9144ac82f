ea9a3b9dc7da372864bc0f04bda62d16952e68ef
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 19:42:54 +0530

    First Commit

---
+++ src/contexts/AuthContext.ts
@@ -0,0 +1,48 @@
+
+import React, { createContext, useState } from 'react';
+import { UserLoginRequest, UserLoginResponse } from '../types/Types';
+import { loginUser } from '../apis/AuthApi';
+
+interface AuthContextProps {
+  user: UserLoginResponse | null;
+  login: (request: UserLoginRequest) => void;
+  logout: () => void;
+}
+
+export const AuthContext = createContext<AuthContextProps>({
+  user: null,
+  login: () => {},
+  logout: () => {},
+});
+
+export const AuthProvider: React.FC = ({ children }) => {
+  const [user, setUser] = useState<UserLoginResponse | null>(null);
+
+  const login = async (request: UserLoginRequest) => {
+    try {
+      console.log('Step 1: Initiating login process...');
+      // Make API call to login user
+      const response = await loginUser(request);
+      console.log('Step 2: Login API call successful. Response:', response);
+
+      // Update user state
+      setUser(response);
+      console.log('Step 3: User login successful. User:', response);
+    } catch (error) {
+      console.error('Error occurred during login:', error);
+    }
+  };
+
+  const logout = () => {
+    console.log('Logging out user...');
+    // Clear user state
+    setUser(null);
+    console.log('User logged out successfully.');
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
