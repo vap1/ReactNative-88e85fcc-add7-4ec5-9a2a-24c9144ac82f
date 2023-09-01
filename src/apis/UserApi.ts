451f25eda787ec99684a13c265d54f5d587bd773
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 19:55:55 +0530

    First Commit

--- src/apis/UserApi.ts
+++ src/apis/UserApi.ts
@@ -3,72 +3,72 @@ import { User, RegistrationFormData, LoginFormData, ProfileFormData } from '../t
 
 // Function to register a new user
 export const registerUser = async (formData: RegistrationFormData): Promise<User> => {
-  console.log('Registering user...');
-  // Add logic to register a new user using backend API
-  // Generate random user data for demonstration purposes
+  console.log('Registering user:', formData);
+
+  // Add registration logic here
+
+  // Generate a random user ID
+  const userId = Math.random().toString();
+
+  // Create a new user object
   const newUser: User = {
-    id: '1',
+    id: userId,
     name: formData.name,
     email: formData.email,
-    password: 'encryptedPassword',
-    profilePicture: 'profilePictureUrl',
-    contactInfo: {
-      phone: '1234567890',
-      email: formData.email,
-    },
-    address: {
-      street: '123 Main St',
-      city: 'New York',
-      state: 'NY',
-      country: 'USA',
-      zipCode: '12345',
-    },
+    password: formData.password,
+    profilePicture: '',
+    contactInfo: { phone: '', email: '' },
+    address: { street: '', city: '', state: '', country: '', zipCode: '' },
   };
-  console.log('User registered successfully!');
+
+  console.log('Registered user:', newUser);
+
   return newUser;
 };
 
-// Function to login a user
+// Function to log in a user
 export const loginUser = async (formData: LoginFormData): Promise<User> => {
-  console.log('Logging in...');
-  // Add logic to authenticate user credentials using backend API
-  // Generate random user data for demonstration purposes
+  console.log('Logging in user:', formData);
+
+  // Add login logic here
+
+  // Generate a random user ID
+  const userId = Math.random().toString();
+
+  // Create a new user object
   const user: User = {
-    id: '1',
+    id: userId,
     name: 'John Doe',
     email: formData.email,
-    password: 'encryptedPassword',
-    profilePicture: 'profilePictureUrl',
-    contactInfo: {
-      phone: '1234567890',
-      email: formData.email,
-    },
-    address: {
-      street: '123 Main St',
-      city: 'New York',
-      state: 'NY',
-      country: 'USA',
-      zipCode: '12345',
-    },
+    password: formData.password,
+    profilePicture: '',
+    contactInfo: { phone: '', email: '' },
+    address: { street: '', city: '', state: '', country: '', zipCode: '' },
   };
-  console.log('Login successful!');
+
+  console.log('Logged in user:', user);
+
   return user;
 };
 
 // Function to update user profile
-export const updateUserProfile = async (formData: ProfileFormData): Promise<User> => {
-  console.log('Updating user profile...');
-  // Add logic to update user profile using backend API
-  // Generate random user data for demonstration purposes
+export const updateUserProfile = async (userId: string, profileData: ProfileFormData): Promise<User> => {
+  console.log('Updating user profile:', profileData);
+
+  // Add profile update logic here
+
+  // Generate a random user ID
   const updatedUser: User = {
-    id: '1',
-    name: formData.name,
-    email: 'johndoe@example.com',
-    password: 'encryptedPassword',
-    profilePicture: 'profilePictureUrl',
-    contactInfo: formData.contactInfo,
-    address: formData.address,
+    id: userId,
+    name: profileData.name,
+    email: '',
+    password: '',
+    profilePicture: profileData.profilePicture,
+    contactInfo: profileData.contactInfo,
+    address: profileData.address,
   };
-  console.log('User profile updated successfully!');
+
+  console.log('Updated user profile:', updatedUser);
+
   return updatedUser;
 };
