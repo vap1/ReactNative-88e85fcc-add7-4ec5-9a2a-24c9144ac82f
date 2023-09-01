c25b897f830a89dd33def92080c821d71d33cc9f
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 19:55:59 +0530

    First Commit

--- src/types/Types.ts
+++ src/types/Types.ts
@@ -52,6 +52,7 @@ export interface ProfileFormData {
   name: string;
   contactInfo: ContactInfo;
   address: Address;
+  profilePicture: string;
 }
 
 // Function to validate email address
@@ -78,55 +79,27 @@ export const decryptPassword = (encryptedPassword: string): string => {
   return encryptedPassword;
 };
 
-// Function to fetch user details by ID
-export const fetchUserDetails = (userId: string): User => {
-  // Add logic to fetch user details from backend API
-  console.log(`Fetching user details for ID: ${userId}`);
-  // Return user details
-  return {
-    id: userId,
-    name: "John Doe",
-    email: "johndoe@example.com",
-    password: "encryptedPassword",
-    profilePicture: "profilePictureUrl",
-    contactInfo: {
-      phone: "1234567890",
-      email: "johndoe@example.com",
-    },
-    address: {
-      street: "123 Main St",
-      city: "New York",
-      state: "NY",
-      country: "USA",
-      zipCode: "12345",
-    },
-  };
+// Function to log user in
+export const login = (email: string, password: string): boolean => {
+  // Add login logic here
+  console.log(`Logging in user with email: ${email}`);
+  // Return true if login is successful, false otherwise
+  return true;
+};
+
+// Function to update user profile
+export const updateProfile = (userId: string, profileData: ProfileFormData): boolean => {
+  // Add profile update logic here
+  console.log(`Updating profile for user with ID: ${userId}`);
+  console.log(`New profile data: ${JSON.stringify(profileData)}`);
+  // Return true if profile update is successful, false otherwise
+  return true;
 };
 
 // Function to fetch all user details for admin
 export const fetchAllUserDetails = (): User[] => {
-  // Add logic to fetch all user details from backend API
+  // Add logic to fetch all user details here
   console.log("Fetching all user details for admin");
   // Return array of user details
-  return [
-    {
-      id: "1",
-      name: "John Doe",
-      email: "johndoe@example.com",
-      password: "encryptedPassword",
-      profilePicture: "profilePictureUrl",
-      contactInfo: {
-        phone: "1234567890",
-        email: "johndoe@example.com",
-      },
-      address: {
-        street: "123 Main St",
-        city: "New York",
-        state: "NY",
-        country: "USA",
-        zipCode: "12345",
-      },
-    },
-    // Add more user details if needed
-  ];
+  return [];
 };
