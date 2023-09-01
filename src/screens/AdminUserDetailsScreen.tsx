b071275ff16efafcd964f9a35ef94417a74a6f42
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 20:09:28 +0530

    First Commit

--- src/screens/AdminUserDetailsScreen.tsx
+++ src/screens/AdminUserDetailsScreen.tsx
@@ -1,50 +1,49 @@
 
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useContext } from 'react';
 import { View, Text } from 'react-native';
-import { fetchAllUserDetails } from '../apis/AdminApi';
+import { AdminContext } from '../contexts/AdminContext';
 import { User } from '../types/Types';
 
 const AdminUserDetailsScreen: React.FC = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const { users, fetchAdminUserDetails, loading, error } = useContext(AdminContext);
 
   useEffect(() => {
-    const getUsers = async () => {
-      try {
-        setIsLoading(true);
-        const fetchedUsers = await fetchAllUserDetails();
-        setUsers(fetchedUsers);
-        setIsLoading(false);
-      } catch (error) {
-        console.error('Error fetching users:', error);
-        setIsLoading(false);
-      }
-    };
-
-    getUsers();
+    console.log('Fetching admin user details...');
+    fetchAdminUserDetails();
   }, []);
 
-  console.log('Users:', users);
+  console.log('Rendering admin user details screen...');
+
+  if (loading) {
+    console.log('Loading admin user details...');
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
+  if (error) {
+    console.log('Error fetching admin user details:', error);
+    return (
+      <View>
+        <Text>Error: {error}</Text>
+      </View>
+    );
+  }
 
   return (
     <View>
-      {isLoading ? (
-        <Text>Loading users...</Text>
-      ) : (
-        <View>
-          {users.length > 0 ? (
-            users.map((user: User) => (
-              <View key={user.id}>
-                <Text>Name: {user.name}</Text>
-                <Text>Email: {user.email}</Text>
-                {/* Add more user details if needed */}
-              </View>
-            ))
-          ) : (
-            <Text>No users found.</Text>
-          )}
+      <Text>Admin User Details</Text>
+      {users.map((user: User) => (
+        <View key={user.email}>
+          <Text>Name: {user.name}</Text>
+          <Text>Email: {user.email}</Text>
+          <Text>Contact Info: {user.contactInfo}</Text>
+          <Text>Address: {user.address}</Text>
+          <Text>Profile Picture: {user.profilePicture}</Text>
         </View>
-      )}
+      ))}
     </View>
   );
 };
