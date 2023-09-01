b2b123738166b36190bcd188a8e64de269afa59b
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 19:42:56 +0530

    First Commit

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,38 @@
+
+import React, { useState } from 'react';
+import { View, TextInput, Button } from 'react-native';
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    console.log('Step 1: User enters email and password');
+    console.log('Email:', email);
+    console.log('Password:', password);
+
+    // Perform login API call here
+    // Add appropriate API call and context usage
+
+    console.log('Step 2: User submits login form');
+  };
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+export default LoginScreen;
