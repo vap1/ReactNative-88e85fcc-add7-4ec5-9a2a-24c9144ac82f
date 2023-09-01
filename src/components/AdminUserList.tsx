2029ab6ad6a36ecefb44cecf41b886f9cdcd4856
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 19:49:12 +0530

    First Commit

--- src/components/AdminUserList.tsx
+++ src/components/AdminUserList.tsx
@@ -1,29 +1,27 @@
 
-import React, { useEffect, useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { View, Text } from 'react-native';
 import { AdminContext } from '../contexts/AdminContext';
-import { User } from '../types/Types';
 
 const AdminUserList: React.FC = () => {
-  const { users, getUsers } = useContext(AdminContext);
+  const { users, fetchAllUsers } = useContext(AdminContext);
 
   useEffect(() => {
-    console.log('Fetching admin user details...');
-    getUsers();
+    console.log('Fetching all users...');
+    fetchAllUsers();
   }, []);
 
-  console.log('Rendering admin user list...');
+  console.log('Rendering AdminUserList component...');
 
   return (
     <View>
       <Text>Admin User List</Text>
-      {users.map((user: User) => (
-        <View key={user.email}>
+      {users.map((user) => (
+        <View key={user.id}>
           <Text>Name: {user.name}</Text>
           <Text>Email: {user.email}</Text>
-          <Text>Contact Info: {user.contactInfo}</Text>
-          <Text>Address: {user.address}</Text>
-          <Text>Profile Picture: {user.profilePicture}</Text>
+          <Text>Phone: {user.contactInfo.phone}</Text>
+          <Text>Address: {user.address.street}, {user.address.city}, {user.address.state}, {user.address.country}, {user.address.zipCode}</Text>
         </View>
       ))}
     </View>
