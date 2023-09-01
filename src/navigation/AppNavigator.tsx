4e826fc05f17b4570f0f3c992c31f48a48f0e4c9
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 19:49:16 +0530

    First Commit

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,42 +1,33 @@
 
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import React, { useContext } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-
-import RegistrationScreen from '../screens/RegistrationScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+import { AuthContext } from '../contexts/AuthContext';
 import LoginScreen from '../screens/LoginScreen';
+import RegistrationScreen from '../screens/RegistrationScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import AdminUserDetailsScreen from '../screens/AdminUserDetailsScreen';
 
 const Stack = createStackNavigator();
 
-const AppNavigator = () => {
-  console.log('Initializing App Navigator');
+const AppNavigator: React.FC = () => {
+  const { user } = useContext(AuthContext);
+
+  console.log('Rendering AppNavigator component...');
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Registration"
-          component={RegistrationScreen}
-          options={{ title: 'Registration' }}
-        />
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ title: 'Login' }}
-        />
-        <Stack.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{ title: 'Profile' }}
-        />
-        <Stack.Screen
-          name="AdminUserDetails"
-          component={AdminUserDetailsScreen}
-          options={{ title: 'Admin User Details' }}
-        />
-      </Stack.Navigator>
+      {user ? (
+        <Stack.Navigator>
+          <Stack.Screen name="Profile" component={ProfileScreen} />
+          <Stack.Screen name="AdminUserDetails" component={AdminUserDetailsScreen} />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Registration" component={RegistrationScreen} />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 };
