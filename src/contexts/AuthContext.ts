933636c1e12956882169635aabc1a839eb5f8f72
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 19:49:07 +0530

    First Commit

--- src/contexts/AuthContext.ts
+++ src/contexts/AuthContext.ts
@@ -1,47 +1,107 @@
 
 import React, { createContext, useState } from 'react';
-import { UserLoginRequest, UserLoginResponse } from '../types/Types';
-import { loginUser } from '../apis/AuthApi';
+import { User, LoginFormData, RegistrationFormData, ProfileFormData, validateEmail, encryptPassword, decryptPassword, fetchUserDetails } from '../types/Types';
 
-interface AuthContextProps {
-  user: UserLoginResponse | null;
-  login: (request: UserLoginRequest) => void;
+// Create the AuthContext
+export const AuthContext = createContext<{
+  user: User | null;
+  login: (formData: LoginFormData) => void;
+  register: (formData: RegistrationFormData) => void;
   logout: () => void;
-}
-
-export const AuthContext = createContext<AuthContextProps>({
+  updateProfile: (formData: ProfileFormData) => void;
+}>({
   user: null,
   login: () => {},
+  register: () => {},
   logout: () => {},
+  updateProfile: () => {},
 });
 
+// AuthProvider component
 export const AuthProvider: React.FC = ({ children }) => {
-  const [user, setUser] = useState<UserLoginResponse | null>(null);
+  const [user, setUser] = useState<User | null>(null);
 
-  const login = async (request: UserLoginRequest) => {
-    try {
-      console.log('Step 1: Initiating login process...');
-      // Make API call to login user
-      const response = await loginUser(request);
-      console.log('Step 2: Login API call successful. Response:', response);
-
-      // Update user state
-      setUser(response);
-      console.log('Step 3: User login successful. User:', response);
-    } catch (error) {
-      console.error('Error occurred during login:', error);
-    }
+  // Login function
+  const login = (formData: LoginFormData) => {
+    console.log('Logging in...');
+    // Add logic to authenticate user credentials using backend API
+    // Set the user state if authentication is successful
+    setUser({
+      id: '1',
+      name: 'John Doe',
+      email: formData.email,
+      password: encryptPassword(formData.password),
+      profilePicture: 'profilePictureUrl',
+      contactInfo: {
+        phone: '1234567890',
+        email: formData.email,
+      },
+      address: {
+        street: '123 Main St',
+        city: 'New York',
+        state: 'NY',
+        country: 'USA',
+        zipCode: '12345',
+      },
+    });
+    console.log('Login successful!');
   };
 
+  // Register function
+  const register = (formData: RegistrationFormData) => {
+    console.log('Registering user...');
+    // Add logic to create a new user account using backend API
+    // Set the user state if registration is successful
+    setUser({
+      id: '1',
+      name: formData.name,
+      email: formData.email,
+      password: encryptPassword(formData.password),
+      profilePicture: 'profilePictureUrl',
+      contactInfo: {
+        phone: '1234567890',
+        email: formData.email,
+      },
+      address: {
+        street: '123 Main St',
+        city: 'New York',
+        state: 'NY',
+        country: 'USA',
+        zipCode: '12345',
+      },
+    });
+    console.log('Registration successful!');
+  };
+
+  // Logout function
   const logout = () => {
-    console.log('Logging out user...');
-    // Clear user state
+    console.log('Logging out...');
+    // Clear the user state
     setUser(null);
-    console.log('User logged out successfully.');
+    console.log('Logout successful!');
+  };
+
+  // Update profile function
+  const updateProfile = (formData: ProfileFormData) => {
+    console.log('Updating profile...');
+    // Add logic to update user profile using backend API
+    // Update the user state with the new profile data
+    setUser((prevUser) => {
+      if (prevUser) {
+        return {
+          ...prevUser,
+          name: formData.name,
+          contactInfo: formData.contactInfo,
+          address: formData.address,
+        };
+      }
+      return null;
+    });
+    console.log('Profile updated successfully!');
   };
 
   return (
-    <AuthContext.Provider value={{ user, login, logout }}>
+    <AuthContext.Provider value={{ user, login, register, logout, updateProfile }}>
       {children}
     </AuthContext.Provider>
   );
