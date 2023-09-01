1cd3e3bc9c0f8262961c1cdb1d52924b2aa3fbc5
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 20:09:36 +0530

    First Commit

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,34 +1,41 @@
 
-import React, { useContext } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
-import { AuthContext } from '../contexts/AuthContext';
-import LoginScreen from '../screens/LoginScreen';
+import { NavigationContainer } from '@react-navigation/native';
+
 import RegistrationScreen from '../screens/RegistrationScreen';
+import LoginScreen from '../screens/LoginScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import AdminUserDetailsScreen from '../screens/AdminUserDetailsScreen';
 
 const Stack = createStackNavigator();
 
-const AppNavigator: React.FC = () => {
-  const { user } = useContext(AuthContext);
-
-  console.log('User:', user);
+const AppNavigator = () => {
+  console.log('Initializing App Navigator');
 
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {user ? (
-          <>
-            <Stack.Screen name="Profile" component={ProfileScreen} />
-            <Stack.Screen name="AdminUserDetails" component={AdminUserDetailsScreen} />
-          </>
-        ) : (
-          <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Registration" component={RegistrationScreen} />
-          </>
-        )}
+        <Stack.Screen
+          name="Registration"
+          component={RegistrationScreen}
+          options={{ title: 'Registration' }}
+        />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: 'Login' }}
+        />
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{ title: 'Profile' }}
+        />
+        <Stack.Screen
+          name="AdminUserDetails"
+          component={AdminUserDetailsScreen}
+          options={{ title: 'Admin User Details' }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
