894630fe08ad1e780118ca13722b9819244e2d80
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 20:02:58 +0530

    First Commit

--- src/apis/AdminApi.ts
+++ src/apis/AdminApi.ts
@@ -2,32 +2,35 @@
 import { User } from '../types/Types';
 
 // Function to fetch all user details for admin
-export const fetchAllUserDetails = (): User[] => {
+export const fetchAllUserDetails = async (): Promise<User[]> => {
   console.log('Fetching all user details for admin');
 
-  // Add logic to fetch all user details here
-  const allUserDetails: User[] = [
+  // Add logic to fetch all user details from backend API
+  // Replace the following code with actual API call
+
+  // Generate random user details
+  const users: User[] = [
     {
       id: '1',
       name: 'John Doe',
       email: 'johndoe@example.com',
-      password: 'password',
-      profilePicture: '',
-      contactInfo: { phone: '1234567890', email: 'johndoe@example.com' },
-      address: { street: '123 Main St', city: 'New York', state: 'NY', country: 'USA', zipCode: '12345' },
+      password: 'encryptedPassword',
+      profilePicture: 'profilePictureUrl',
+      contactInfo: {
+        phone: '1234567890',
+        email: 'johndoe@example.com',
+      },
+      address: {
+        street: '123 Main St',
+        city: 'New York',
+        state: 'NY',
+        country: 'USA',
+        zipCode: '12345',
+      },
     },
-    {
-      id: '2',
-      name: 'Jane Smith',
-      email: 'janesmith@example.com',
-      password: 'password',
-      profilePicture: '',
-      contactInfo: { phone: '9876543210', email: 'janesmith@example.com' },
-      address: { street: '456 Elm St', city: 'Los Angeles', state: 'CA', country: 'USA', zipCode: '67890' },
-    },
+    // Add more user details if needed
   ];
 
-  console.log('All user details:', allUserDetails);
-
-  return allUserDetails;
+  console.log('Fetched all user details:', users);
+  return users;
 };
