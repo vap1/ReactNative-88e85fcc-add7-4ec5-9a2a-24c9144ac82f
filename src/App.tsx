428bbc1afcb21009899019f32ededb136f1ba5c6
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 20:09:33 +0530

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,38 @@
 
-import React, { useContext } from 'react';
+import React, { useEffect } from 'react';
+import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import { AuthContext } from '../contexts/AuthContext';
-import LoginScreen from '../screens/LoginScreen';
-import RegistrationScreen from '../screens/RegistrationScreen';
-import ProfileScreen from '../screens/ProfileScreen';
-import AdminUserDetailsScreen from '../screens/AdminUserDetailsScreen';
+import { AuthContextProvider } from './contexts/AuthContext';
+import { ProfileContextProvider } from './contexts/ProfileContext';
+import { AdminContextProvider } from './contexts/AdminContext';
+import AppNavigator from './navigation/AppNavigator';
 
 const Stack = createStackNavigator();
 
-const App: React.FC = () => {
-  const { user } = useContext(AuthContext);
+const App = () => {
+  useEffect(() => {
+    console.log('App started');
+    // Additional initialization logic can be added here
+  }, []);
 
-  console.log('User:', user);
+  console.log('Initializing App Navigator');
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        {user ? (
-          <>
-            <Stack.Screen name="Profile" component={ProfileScreen} />
-            <Stack.Screen name="AdminUserDetails" component={AdminUserDetailsScreen} />
-          </>
-        ) : (
-          <>
-            <Stack.Screen name="Login" component={LoginScreen} />
-            <Stack.Screen name="Registration" component={RegistrationScreen} />
-          </>
-        )}
-      </Stack.Navigator>
+      <AuthContextProvider>
+        <ProfileContextProvider>
+          <AdminContextProvider>
+            <Stack.Navigator>
+              <Stack.Screen
+                name="AppNavigator"
+                component={AppNavigator}
+                options={{ headerShown: false }}
+              />
+            </Stack.Navigator>
+          </AdminContextProvider>
+        </ProfileContextProvider>
+      </AuthContextProvider>
     </NavigationContainer>
   );
 };
