0be4370bb292c4679630580c264d7262ccf29708
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 19:49:06 +0530

    First Commit

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,34 +1,86 @@
 
-import React, { useEffect, useContext } from 'react';
-import { View, Text } from 'react-native';
-import { UserProfileRequest, UserProfileResponse } from '../types/Types';
+import React, { useContext, useState } from 'react';
+import { View, TextInput, Button, Alert } from 'react-native';
+import { ProfileFormData, User } from '../types/Types';
 import { ProfileContext } from '../contexts/ProfileContext';
+import { updateUserProfile } from '../apis/ProfileApi';
 
 const ProfileScreen: React.FC = () => {
-  const { userProfile, getUserProfile } = useContext(ProfileContext);
+  const { user, updateUserProfile } = useContext(ProfileContext);
+  const [formData, setFormData] = useState<ProfileFormData>({
+    name: user?.name || '',
+    contactInfo: user?.contactInfo || { phone: '', email: '' },
+    address: user?.address || { street: '', city: '', state: '', country: '', zipCode: '' },
+  });
 
-  useEffect(() => {
-    console.log('Fetching user profile...');
-    const request: UserProfileRequest = {
-      token: 'user_token', // Replace with actual user token
-    };
-    getUserProfile(request)
-      .then((response: UserProfileResponse) => {
-        console.log('User profile fetched successfully:', response.user);
-      })
-      .catch((error: Error) => {
-        console.log('Error fetching user profile:', error.message);
-      });
-  }, []);
+  const handleInputChange = (key: keyof ProfileFormData, value: string) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [key]: value,
+    }));
+  };
+
+  const handleUpdateProfile = async () => {
+    console.log('Updating user profile...');
+
+    try {
+      // Call the updateUserProfile API to update the user profile
+      const updatedUser: User = await updateUserProfile(formData);
+      console.log('User profile updated successfully:', updatedUser);
+
+      // Show success message to the user
+      Alert.alert('Profile updated', 'Your profile has been updated successfully!');
+    } catch (error) {
+      console.error('Error updating user profile:', error);
+
+      // Show error message to the user
+      Alert.alert('Profile update failed', 'An error occurred while updating your profile. Please try again later.');
+    }
+  };
 
   return (
     <View>
-      <Text>User Profile:</Text>
-      <Text>Name: {userProfile.name}</Text>
-      <Text>Email: {userProfile.email}</Text>
-      <Text>Contact Info: {userProfile.contactInfo}</Text>
-      <Text>Address: {userProfile.address}</Text>
-      <Text>Profile Picture: {userProfile.profilePicture}</Text>
+      <TextInput
+        placeholder="Name"
+        value={formData.name}
+        onChangeText={(value) => handleInputChange('name', value)}
+      />
+      <TextInput
+        placeholder="Phone"
+        value={formData.contactInfo.phone}
+        onChangeText={(value) => handleInputChange('contactInfo', { ...formData.contactInfo, phone: value })}
+      />
+      <TextInput
+        placeholder="Email"
+        value={formData.contactInfo.email}
+        onChangeText={(value) => handleInputChange('contactInfo', { ...formData.contactInfo, email: value })}
+      />
+      <TextInput
+        placeholder="Street"
+        value={formData.address.street}
+        onChangeText={(value) => handleInputChange('address', { ...formData.address, street: value })}
+      />
+      <TextInput
+        placeholder="City"
+        value={formData.address.city}
+        onChangeText={(value) => handleInputChange('address', { ...formData.address, city: value })}
+      />
+      <TextInput
+        placeholder="State"
+        value={formData.address.state}
+        onChangeText={(value) => handleInputChange('address', { ...formData.address, state: value })}
+      />
+      <TextInput
+        placeholder="Country"
+        value={formData.address.country}
+        onChangeText={(value) => handleInputChange('address', { ...formData.address, country: value })}
+      />
+      <TextInput
+        placeholder="Zip Code"
+        value={formData.address.zipCode}
+        onChangeText={(value) => handleInputChange('address', { ...formData.address, zipCode: value })}
+      />
+      <Button title="Update Profile" onPress={handleUpdateProfile} />
     </View>
   );
 };
