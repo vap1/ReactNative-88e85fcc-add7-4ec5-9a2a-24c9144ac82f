9d2346457b650d45a628db11d75af33e728414f0
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 20:02:59 +0530

    First Commit

--- src/screens/AdminUserDetailsScreen.tsx
+++ src/screens/AdminUserDetailsScreen.tsx
@@ -1,19 +1,50 @@
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text } from 'react-native';
 import { fetchAllUserDetails } from '../apis/AdminApi';
 import { User } from '../types/Types';
 
 const AdminUserDetailsScreen: React.FC = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    console.log('Fetching all user details for admin');
-    const allUserDetails: User[] = fetchAllUserDetails();
-    console.log('All user details:', allUserDetails);
+    const getUsers = async () => {
+      try {
+        setIsLoading(true);
+        const fetchedUsers = await fetchAllUserDetails();
+        setUsers(fetchedUsers);
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching users:', error);
+        setIsLoading(false);
+      }
+    };
+
+    getUsers();
   }, []);
+
+  console.log('Users:', users);
 
   return (
     <View>
-      <Text>Admin User Details</Text>
+      {isLoading ? (
+        <Text>Loading users...</Text>
+      ) : (
+        <View>
+          {users.length > 0 ? (
+            users.map((user: User) => (
+              <View key={user.id}>
+                <Text>Name: {user.name}</Text>
+                <Text>Email: {user.email}</Text>
+                {/* Add more user details if needed */}
+              </View>
+            ))
+          ) : (
+            <Text>No users found.</Text>
+          )}
+        </View>
+      )}
     </View>
   );
 };
