95cefb214f73e34b11d5f0b0a9fe31935fdde662
vap1 <vaibhavvasant1705@gmail.com>
2023-09-01 20:03:07 +0530

    First Commit

--- src/contexts/AdminContext.ts
+++ src/contexts/AdminContext.ts
@@ -1,35 +1,50 @@
 
-import React, { createContext } from 'react';
-import { User, fetchAllUserDetails } from '../types/Types';
+import React, { createContext, useState } from 'react';
+import { User } from '../types/Types';
 
 // Create the AdminContext
 export const AdminContext = createContext<{
   users: User[];
-  fetchAllUserDetails: () => void;
+  fetchAllUsers: () => void;
 }>({
   users: [],
-  fetchAllUserDetails: () => {},
+  fetchAllUsers: () => {},
 });
 
 // Create the AdminProvider component
 export const AdminProvider: React.FC = ({ children }) => {
-  // Function to fetch all user details for admin
-  const handleFetchAllUserDetails = () => {
-    console.log('Fetching all user details for admin');
+  const [users, setUsers] = useState<User[]>([]);
 
-    // Add logic to fetch all user details here
-    const allUserDetails = fetchAllUserDetails();
-
-    console.log('All user details:', allUserDetails);
+  // Function to fetch all user details
+  const fetchAllUsers = () => {
+    console.log('Fetching all user details');
+    // Add logic to fetch all user details from backend API
+    // Set the users state with the fetched data
+    setUsers([
+      {
+        id: '1',
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: 'encryptedPassword',
+        profilePicture: 'profilePictureUrl',
+        contactInfo: {
+          phone: '1234567890',
+          email: 'johndoe@example.com',
+        },
+        address: {
+          street: '123 Main St',
+          city: 'New York',
+          state: 'NY',
+          country: 'USA',
+          zipCode: '12345',
+        },
+      },
+      // Add more user details if needed
+    ]);
   };
 
   return (
-    <AdminContext.Provider
-      value={{
-        users: [],
-        fetchAllUserDetails: handleFetchAllUserDetails,
-      }}
-    >
+    <AdminContext.Provider value={{ users, fetchAllUsers }}>
       {children}
     </AdminContext.Provider>
   );
